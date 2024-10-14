ce122df2daf3a275c88288d7b3c2a7a5539ae502
iluengas <isaac.j.luengas@gmail.com>
2024-10-13 19:28:31 -0700

    deploying to github pages

--- app/utils/imageHelper.js
+++ app/utils/imageHelper.js
@@ -1,6 +1,5 @@
 export const getImageUrl = (imagePath) => {
-  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/images/${imagePath}`;
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}images/${imagePath}`;
   console.log("Generated Image URL:", url);
   return `${process.env.NEXT_PUBLIC_BASE_URL}images/${imagePath}`;
 };
-
